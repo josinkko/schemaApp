ef45057245a1cc3a6fa9f223cc03d53e3edaa5e3
PatrikSundback <patrik.sundback@gmail.com>
2013-09-12 12:03:43 +0200

    Curicullum v1.0 Basic Structure integrated With Interface

---
+++ schemApp/MessageStudentViewController.m
@@ -0,0 +1,82 @@
+//
+//  MessageStudentViewController.m
+//  schemApp
+//
+//  Created by sebastian holmqvist on 2013-09-09.
+//  Copyright (c) 2013 sebastian holmqvist. All rights reserved.
+//
+
+#import "MessageStudentViewController.h"
+
+@interface MessageStudentViewController ()  <UITextFieldDelegate, UISearchBarDelegate>
+
+@end
+
+@implementation MessageStudentViewController
+@synthesize MessageText;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [self timeStamp];
+    [super viewDidLoad];
+    MessageText.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    MessageText.layer.borderWidth = 0.5f;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)Back:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)send:(id)sender {
+    
+}
+
+- (IBAction)Clear:(id)sender {
+    [self textViewDidBeginEditing:MessageText];
+
+}
+
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+#pragma mark reload textfield
+- (void)textViewDidBeginEditing:(UITextView *)textView
+{
+    
+    textView.text = @"";
+    [self timeStamp];
+}
+-(void)timeStamp
+{
+#pragma mark Set time stamp on message
+    NSDate *currentDateTime = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEE, HH:mm"];
+    NSString *dateInStringFormated = [dateFormatter stringFromDate:currentDateTime];
+    NSLog(@"%@", dateInStringFormated);
+    MessageText.text = dateInStringFormated;
+}
+
+@end

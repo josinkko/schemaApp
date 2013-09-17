d2f93a0978264acc78bf879ce192a6316e06b60d
Sebastian <sebastian_holmqvist@hotmail.com>
2013-09-17 11:36:21 +0200

    sebastian student aut added

--- schemApp/MessageStudentViewController.m
+++ schemApp/MessageStudentViewController.m
@@ -84,7 +84,7 @@
 {
     NSDate *currentDateTime = [NSDate date];
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"EEE, HH:mm"];
+    [dateFormatter setDateFormat:@"EEE, HH:mm \n\n"];
     NSString *dateInStringFormated = [dateFormatter stringFromDate:currentDateTime];
     NSLog(@"%@", dateInStringFormated);
     MessageText.text = dateInStringFormated;
